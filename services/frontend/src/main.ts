96ec062a27a48b8d19bbd96493fe1af91f273e8d
Abhinav Sirohi <abhinavnj@gmail.com>
2024-08-12 16:10:06 -0400

    new frontend

--- services/frontend/src/main.ts
+++ services/frontend/src/main.ts
@@ -1,17 +1,11 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
-import axios from 'axios';
-axios.defaults.withCredentials = true;
-axios.defaults.baseURL = 'http://localhost:5001/';  // the FastAPI backend
-
+// import './style.css'
 import App from './App.vue'
-import router from './router'
+import './index.css'
 
+const pinia = createPinia()
 const app = createApp(App)
 
-app.use(createPinia())
-app.use(router)
+app.use(pinia)
 app.mount('#app')
