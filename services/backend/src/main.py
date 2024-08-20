213aca1f16d62150fa077f1dfa0a2963599bf170
Abhinav Sirohi <abhinavnj@gmail.com>
2024-08-20 16:15:46 -0400

    allow ec2 8080

--- services/backend/src/main.py
+++ services/backend/src/main.py
@@ -9,7 +9,7 @@ app = FastAPI()
 
 app.add_middleware(
   CORSMiddleware,
-  allow_origins=["http://localhost:8080", "http://localhost:5173"],
+  allow_origins=["http://localhost:8080", "http://localhost:5173", "http://3.22.166.72:8080"],
   allow_credentials=True,
   allow_methods=["*"],
   allow_headers=["*"],
