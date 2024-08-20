419231ffc41455f3bb2ae26309b551d42525f624
Abhinav Sirohi <abhinavnj@gmail.com>
2024-08-20 16:23:00 -0400

    use ec2 backend vite

--- services/frontend/src/main.ts
+++ services/frontend/src/main.ts
@@ -9,7 +9,7 @@ const pinia = createPinia()
 const app = createApp(App)
 
 axios.defaults.withCredentials = true;
-axios.defaults.baseURL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:5001/';
+axios.defaults.baseURL = import.meta.env.VITE_API_BASE_URL || 'http://3.22.166.72:5001/';
 
 app.use(pinia)
 app.use(router)
